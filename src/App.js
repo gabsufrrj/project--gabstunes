a774f57eca92b953d5244c83d25f29995d9708c0
Gabriel Gregório <gabrielgregorio@pop-os.localdomain>
2022-03-07 15:29:10 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
+import Profile from './pages/Profile';
+import NotFound from './pages/NotFound';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes!</p>);
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Login } />
+          <Route exact path="/search" component={ Search } />
+          <Route exact path="/album/:id" component={ Album } />
+          <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route exact path="*" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
